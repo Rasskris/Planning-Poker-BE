f4cd1de89b024df34f30cee2e348a0a00121b43e
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-05 18:11:05 +0300

    refactor: change logic operators to nullish coalescing operator

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -3,8 +3,8 @@ import { HttpException } from '../exceptions';
 import { SERVER_ERROR_MESSAGE, SERVER_ERROR_STATUS_CODE } from '../constants';
 
 const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
-  const status = error.status || SERVER_ERROR_STATUS_CODE;
-  const message = error.message || SERVER_ERROR_MESSAGE;
+  const status = error.status ?? SERVER_ERROR_STATUS_CODE;
+  const message = error.message ?? SERVER_ERROR_MESSAGE;
   response
     .status(status)
     .send({
