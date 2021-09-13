5e9c117b6c99d40d349cdf2c68fec44b9a9d7cd2
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-13 17:50:41 +0300

    PPS-02: add mongo and socket initializators

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,34 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { createServer, Server as HttpServer } from "http";
+import { Server } from "socket.io";
 import { json, urlencoded } from 'body-parser';
+import mongoose from 'mongoose';
 import { errorMiddleware, loggerMiddleware } from './middleware';
+import { onConnection } from './socket';
+import { UserController, GameController, MessageController, IssueController } from './controllers';
 
 class App {
   public app: express.Application;
   public port: number;
+  public server: HttpServer;
+  public ioServer: Server;
 
   constructor(port: number) {
     this.app = express();
     this.port = port;
+    this.server = createServer(this.app);
+    this.ioServer = new Server(this.server);
 
     this.initializeMiddlewares();
+    this.initializeMongoDB();
+    this.initializeSocketConnection();
+    this.initializeControllers();
   }
 
   public listen() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log(`App listening on the port ${this.port}`);
     })
   }
@@ -27,6 +40,33 @@ class App {
       .use(cors())
       .use(errorMiddleware)
       .use(loggerMiddleware);
+  }
+
+  private initializeMongoDB() {
+    const { MONGO_USER, MONGO_PASSWORD, MONGO_URL } = process.env;
+
+    mongoose.Promise = global.Promise;
+    mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_URL}`);
+    mongoose.set('toJSON', {
+      virtuals: true,
+    });
+  }
+
+  private initializeControllers() {
+    const controllers = [
+      new GameController(),
+      new UserController(),
+      new MessageController(),
+      new IssueController(),
+    ];
+
+    controllers.forEach((controller) => {
+      this.app.use('/api', controller.router);
+    });
+  }
+
+  public initializeSocketConnection() {
+    this.ioServer.on('connection', onConnection);
   }
 }
 
