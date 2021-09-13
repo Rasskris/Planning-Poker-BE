e8bf6d034d0daf539b51652baf3136c06a1961c0
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-13 17:45:48 +0300

    PPS-02: add interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { Document } from 'mongoose';
+
+export interface User extends Document {
+  _id: string;
+  firstName: string;
+  lastName?: string;
+  jobPosition?: string;
+  role: string;
+  image?: string;
+  gameId: string;
+}
+
+export interface Game extends Document {
+  _id: string;
+  creatorId?: string;
+}
+
+export interface Issue extends Document {
+  _id: string;
+  title: string;
+  priority: string;
+  creatorId: string;
+  gameId: string;
+  done?: boolean;
+}
+
+export interface Message extends Document {
+  _id: string;
+  text: string;
+  sender: User;
+  gameId: string;
+}
+
+export interface Controller {
+  path: string;
+  router: Router;
+}
