8fdbb7be3b53f96a82d2d38d8095c3252c5ed6ee
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-13 17:48:35 +0300

    PPS-02: add index files fro export

---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+export { GameController } from './GameController';
+export { UserController } from './UserController';
+export { IssueController } from './IssueController';
+export { MessageController } from './MessageController';
