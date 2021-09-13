aca9be9cfffa30d38ee0173ac689ea74c25141dd
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-13 17:46:40 +0300

    PPS-02: add constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,21 @@
-export const PORT = 8000;
+export const PORT = 5000;
 
 export const SERVER_ERROR_STATUS_CODE = 500;
 
 export const SERVER_ERROR_MESSAGE = 'Something went wrong';
+
+export const HALF_OF_USERS = 50;
+
+export const ERROR_OF_FETCH = 'Fetching failed';
+
+export const ERROR_OF_SAVE = 'Saving failed';
+
+export const ERROR_OF_DELETE = 'Delete failed';
+
+export const ERROR_OF_UPDATE = 'Update failed';
+
+export const userRoles = {
+  player: 'player',
+  dealer: 'dealer',
+  observer: 'observer',
+};
