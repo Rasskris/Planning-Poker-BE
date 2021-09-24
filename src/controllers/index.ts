bfd75f70505a01a4a8a375f74ebf18e7868d0c55
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-24 13:58:16 +0300

    PPS-06-refactor-socketEmits-vote: add new line to utils

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export { GameController } from './GameController';
 export { UserController } from './UserController';
 export { IssueController } from './IssueController';
 export { MessageController } from './MessageController';
+export { VoteController } from './VoteContrroller';
--- src/utils.ts
+++ src/utils.ts
@@ -15,5 +15,6 @@ export const getPercentVotedForKick = (countUsers: number, countVotesFor: number
 
 export const checkExistGame = (games: Game[], gameId: string) => {
   const existGame = games.find((game) => game.id === gameId);
+
   return existGame ? true : false;
 };
