4f5d349659203f69671add593026055fa846de24
Rasskris <rassoshenko.kristina@yandex.ru>
2021-10-03 17:03:32 +0300

    PPS-11-fix: change util

--- src/utils/checkExistGame.ts
+++ src/utils/checkExistGame.ts
@@ -3,5 +3,5 @@ import { Game } from '../interfaces';
 export const checkExistGame = (games: Game[], gameId: string) => {
   const existGame = games.find((game) => game.id === gameId);
 
-  return existGame ? true : false;
+  return !!existGame;
 };
