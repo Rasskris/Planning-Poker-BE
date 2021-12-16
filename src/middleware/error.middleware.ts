4340b8a7ae1c68072d4cdbe2c226b3ad184bdb74
Rasskris <rassoshenko.kristina@yandex.ru>
2021-12-16 16:50:40 +0300

    PPS-17 refactor: change error middleware

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,16 +1,13 @@
-import { Request, Response, NextFunction, response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { HttpException } from '../exceptions';
 import { SERVER_ERROR_MESSAGE, SERVER_ERROR_STATUS_CODE } from '../constants';
 
 const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
   const status = error.status ?? SERVER_ERROR_STATUS_CODE;
   const message = error.message ?? SERVER_ERROR_MESSAGE;
-  response
+  res
     .status(status)
-    .send({
-      message,
-      status,
-    });
+    .send({ status, message });
 };
 
 export { errorMiddleware };
